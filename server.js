0682133e752261614aaf13c7eca01f06b10dd4f6
ejaxon <eric@deepweave.com>
2016-12-31 14:29:54 -0500

    Basic framework for express server + client app

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+app.set('port', (process.env.PORT || 3001));
+
+// Express only serves static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.get('/api/food', (req, res) => {
+  const param = req.query.q;
+
+  if (!param) {
+    res.json({
+      error: 'Missing required parameter `q`',
+    });
+    return;
+  }
+
+  res.json({title: "NC Reentry Resources Hub"});
+
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+});
