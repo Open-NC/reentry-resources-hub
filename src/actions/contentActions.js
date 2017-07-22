69686019121b8c1e8ab537987b25e6f800e0fb89
Forrest Guilloud <fbguillo@gmail.com>
2017-07-22 13:07:40 -0400

    Linting contentActions and reducers.

--- src/actions/contentActions.js
+++ src/actions/contentActions.js
@@ -1,53 +1,53 @@
-import * as types from './actionTypes';
 import fetch from 'isomorphic-fetch';
+import * as types from './actionTypes';
 require('es6-promise').polyfill();
 
 export function loadContentSuccess(content) {
-  return { type: types.LOAD_CONTENT_SUCCESS, content };
+  return {
+    type: types.LOAD_CONTENT_SUCCESS,
+    content,
+  };
 }
 
 export function loadMainContentSuccess(content) {
-  return { type: types.LOAD_MAIN_CONTENT_SUCCESS, content };
+  return {
+    type: types.LOAD_MAIN_CONTENT_SUCCESS,
+    content,
+  };
 }
 
 // This is a thunk
 export function loadContent(jurisdiction, topic) {
-  return dispatch => {
+  return (dispatch) => {
     fetch(`/api/${jurisdiction}/${topic}`)
       .then((response) => {
         if (response.status >= 400) {
           throw new Error('Bad response from server');
         }
-        console.log('Layout response');
-        console.log(response);
+
         return response.json();
       })
       .then((content) => {
-        console.log('Layout content');
-        console.log(content);
         dispatch(loadContentSuccess(content));
       });
-  }
+  };
 }
 
 // This is also a thunk
 export function loadMainContent() {
-  return dispatch => {
+  return (dispatch) => {
     fetch('/api/main')
       .then((response) => {
         if (response.status >= 400) {
           throw new Error('Bad response from server');
         }
-        console.log('Layout response');
-        console.log(response);
+
         return response.json();
       })
       .then((content) => {
-        console.log('Layout content');
-        console.log(content);
         dispatch(loadMainContentSuccess(content));
       });
-  }
+  };
 }
 
-//TODO: Create dispatch action for on failure requestContent
+// TODO: Create dispatch action for on failure requestContent
