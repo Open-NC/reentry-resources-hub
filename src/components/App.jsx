d27d74738f6fd1af999719580b871d8046f96019
Blake Guilloud <blake.guilloud@gmail.com>
2017-08-20 17:54:11 -0400

    Linting most of project. Leaving CountyModal as is.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { snapshot } from 'react-snapshot';
 import Header from './Header.jsx';
 import Content from './Content.jsx';
 import Footer from './Footer.jsx';
-import { snapshot } from 'react-snapshot';
 
 export default class App extends Component {
   state = {
-    content: null
+    content: null,
   };
-
-  updateContent({params}) {
-    const p = snapshot(function() {
-      return fetch(`/api/${params.jurisdiction}/${params.topic}`).then(response => response.ok ? response.json() : Promise.reject(null));
-    }).then((content) => this.setState({content}));
-
-    // this works around a bug in snapshot where it doesn't return a promise with a .catch() method
-    p && p.catch(e => console.log(e));
-  }
 
   componentWillMount() {
     this.updateContent(this.props.match);
@@ -27,6 +18,19 @@ export default class App extends Component {
     if (this.props.match !== nextProps.match) {
       this.updateContent(nextProps.match);
     }
+  }
+
+  updateContent({ params }) {
+    const p = snapshot(() => {
+      const url = `/api/${params.jurisdiction}/${params.topic}`;
+
+      return fetch(url)
+        .then(response => (response.ok ? response.json() : Promise.reject(null)));
+    })
+    .then(content => this.setState({ content }));
+
+    // this works around a bug in snapshot where it doesn't return a promise with a .catch() method
+    p && p.catch(e => console.log(e)); // eslint-disable-line
   }
 
   render() {
