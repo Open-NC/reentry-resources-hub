3336045ee449d046ec0e05366456de4400f8c677
Eric Jackson <eric@deepweave.com>
2018-01-20 12:35:41 -0500

    Cleaning up

--- server/index.js
+++ server/index.js
@@ -5,12 +5,12 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 require('dotenv').config();
 const { compose } = require('./compose');
-const dbCompose = require('./db_compose').compose;
 const controller = require('./controller');
 console.log(process.env.dbhost);
 
 const app = express();
 
+// NOTE: NOT SURE THE BELOW IS RELEVANT ANYMORE
 // Checking for if in dev mode or in prod.
 // Better way of doing this would to be setting a NODE_ENV variable on app load.
 if (!process.env.AWS_KEY) {
@@ -23,26 +23,10 @@ app.use(cors());
 
 app.set('port', (process.env.PORT || 3001));
 
-const doDb = true;
 app.get('/api/:jurisdiction/:topic', (req, res) => {
-  let contentDirectory = '../nc-reentry-resources-content';
-  if (process.env.content_directory) {
-    contentDirectory = process.env.content_directory;
-  }
-  contentDirectory = '.';
-  if (!doDb) {
-    compose(req.params.jurisdiction, req.params.topic,
-      contentDirectory, (content) => {
-        console.log(JSON.stringify(content));
-        res.json(content);
-      });
-  } else {
-    console.log('Calling dbCompose');
-    dbCompose(req.params.jurisdiction, req.params.topic,
-    contentDirectory, (content) => {
-      res.json(content);
-    });
-  }
+  compose(req.params.jurisdiction, req.params.topic, (content) => {
+    res.json(content);
+  });
 });
 
 app.post('/api/sendEmail', controller.sendEmail);
