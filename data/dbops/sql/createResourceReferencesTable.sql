559c0bb65b04e154ba14d4b86181adfad247fab9
Melanie Mazanec <melanie.mazanec@gmail.com>
2018-08-26 21:15:09 -0400

    Remove server and add graphql

--- data/dbops/sql/createResourceReferencesTable.sql
+++
@@ -1,8 +0,0 @@
-create table if not exists resource_references (
-  id varchar(36) primary key,
-  resource varchar(36) not null,
-  topic varchar(36) not null,
-  ordinal integer
-);
-truncate table resource_references;
-select 'OK' as result;
