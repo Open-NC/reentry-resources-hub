91f622b3769992c0d761d3596f0847f139af2811
Melanie Mazanec <melanie.mazanec@gmail.com>
2018-10-01 21:23:09 -0400

    List services

--- src/CountyPage.js
+++ src/CountyPage.js
@@ -40,10 +40,33 @@ const CountyPage = (props) => (
           console.log(error);
           return <div className="page-text"> Error :( </div>;
         }
-        return <div
-          dangerouslySetInnerHTML={{__html: data.page.common_content}}
-          className="page-text"
-        >
+        return <div className="page-text" >
+          <h1>About {data.page.taxonomy} in {data.page.location} County</h1>
+          <div dangerouslySetInnerHTML={{__html: data.page.common_content}}></div>
+          {(data.page.local_services.length > 0 || data.page.localized_services.length > 0)
+            && (<div>
+            <h2>{data.page.taxonomy} Resources in {data.page.location} County</h2>
+            <ul>
+              {data.page.local_services.concat(data.page.localized_services).map(localishService => (
+                <li key={`local-${localishService.name}`}>
+                  <a rel="noopener noreferrer" target="_blank" href={localishService.url}>{localishService.name}</a>
+                </li>
+              ))}
+            </ul>
+          </div>)}
+          {data.page.common_services.length > 0 && (<div>
+            <h2>{data.page.taxonomy} Resources in North Carolina</h2>
+            <ul>
+              {data.page.common_services.map(commonService => (
+                <li key={`common-${commonService.name}`}>
+                  <a rel="noopener noreferrer" target="_blank" href={commonService.url}>{commonService.name}</a>
+                </li>
+              ))}
+            </ul>
+          </div>)}
+
+          <ul>
+          </ul>
         </div>
       }}
   </Query>
